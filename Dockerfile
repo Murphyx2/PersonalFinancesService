bcb699e819befef4d1ad1af73a1d32acd318892e
BraulioCespedes <braulio@fullstacklabs.co>
2025-03-25 19:53:41 -0400

    Basic structure, Working on local docker instance.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
-FROM ubuntu:latest
-LABEL authors="TUF"
+FROM maven:3.9.9-eclipse-temurin-21 AS build
+WORKDIR /app
+COPY pom.xml .
+COPY src ./src
+RUN mvn clean package -DskipTests
 
-ENTRYPOINT ["top", "-b"]
+FROM openjdk:21-jdk-slim
+WORKDIR /app
+COPY target/PersonalFinancesService-0.0.1-SNAPSHOT.jar app.jar
+COPY .env .
+EXPOSE ${SERVER_PORT}
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/app/personalfinancesservice/PersonalFinancesServiceApplication.java
+++ src/main/java/com/app/personalfinancesservice/PersonalFinancesServiceApplication.java
@@ -1,13 +1,29 @@
 package com.app.personalfinancesservice;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PersonalFinancesServiceApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(PersonalFinancesServiceApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(PersonalFinancesServiceApplication.class, args);
+		Dotenv dotenv = Dotenv.configure().directory("./").load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication app = new SpringApplication(PersonalFinancesServiceApplication.class);
+		ConfigurableApplicationContext context = app.run(args);
+
+		logger.info("Active profiles: " + Arrays.toString(context.getEnvironment().getActiveProfiles()));
 	}
 
 }
