d955849cd7a367b6265d05cf7392a2791b8ef925
BraulioCespedes <braulio@fullstacklabs.co>
2025-03-24 23:34:42 -0400

    First

---
+++ src/main/java/com/app/personalfinancesservice/PersonalFinancesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.personalfinancesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/personalfinancesservice/PersonalFinancesServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.app.personalfinancesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancesServiceApplicationTests {
+
+}
